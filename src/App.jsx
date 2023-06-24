21fcd7515d1f6be1f92291490b37e4d1eae944a2
fernando <lufeloza@gmail.com>
2023-06-23 20:38:17 -0500

    Toco Arreglar main

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
 import './App.css'
-import Counter from './components/Counter'
-import Foco from './components/Foco'
+
 function App() {
   return (
     <>
-      <Counter></Counter>
-      <Counter></Counter>
-      <Counter></Counter>
-      <Foco></Foco>
-     
+          
       
 
     </>
