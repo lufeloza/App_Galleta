9a527688d50b27292937f2eefa677698906c004c
fernando <lufeloza@gmail.com>
2023-06-28 11:44:44 -0500

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,22 @@ import PhrasesCard from './components/PhrasesCard';
 import { useState } from 'react';
 
 function App() {
-  
+  const fondos = ['url(img/fondo1.png)', 'url(img/fondo2.png)', 'url(img/fondo3.png)', 'url(img/fondo4.png)', 'url(img/fondo5.png)', 'url(img/fondo6.png)', 'url(img/fondo7.png)', 'url(img/fondo8.png)']
   const [index, setIndex] = useState(0);
 
   const changeUser = () => {
     const nuevoIndex = Math.floor(Math.random()*(phrases.length -1))
-    const nuevoFondo = Math.floor(Math.random()*3)
+    const nuevoFondo = Math.floor(Math.random()*7)
     console.log(nuevoIndex);
     if (nuevoIndex < phrases.length - 1) {
       setIndex(nuevoIndex);
     } else {
       setIndex(0);
     }
+    document.body.style = `background-image:${fondos[nuevoFondo]};`
   };
   
-  document.body.style = `background-image: url(img/fondo2.png);`
+  
 
   return (
     <>
